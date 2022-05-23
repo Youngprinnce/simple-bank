7201ad3f67e8943facf5bc86cb2fdc31b4a677d3
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-05-23 22:55:54 +0100

    fix deadloack

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -7,29 +7,35 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TextTransferTx(t *testing.T){
+func TextTransferTxDeadlock(t *testing.T){
 	store := NewStore(testDB)
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 
 	// run a conconrrent transfer transactions
-	n := 5
+	n := 10
 	amount := int64(10)
 
 	errs := make(chan error)
-	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i % 2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
 		go func(){
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
-				FromAccountID: account1.ID,
-				ToAccountID: account2.ID,
+			ctx := context.Background()
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID: toAccountID,
 				Amount: amount,
 			})
 
 			errs <- err
-			results <- result
 		}()
 	}
 	
@@ -38,41 +44,15 @@ func TextTransferTx(t *testing.T){
 	for i := 0; i< n; i++ {
 		err := <- errs
 		require.NoError(t, err)
-
-		result := <-results
-		require.NotEmpty(t, result)
-
-		// check transfer
-		transfer := result.Transfer
-		require.NotEmpty(t, transfer)
-		require.Equal(t, account1.ID, transfer.FromAccountID)
-		require.Equal(t, account2.ID, transfer.ToAccountID)
-		require.Equal(t, amount, transfer.Amount)
-		require.NotZero(t, transfer.ID)
-		require.NotZero(t, transfer.CreatedAt)
-
-		_, err = store.GetTransfer(context.Background(), transfer.ID)
-		require.NoError(t, err)
-
-		// entries
-		fromEntry := result.FromEntry
-		require.NotEmpty(t, fromEntry)
-		require.Equal(t,account1.ID, fromEntry.AccountID)
-		require.Equal(t, -amount, fromEntry.Amount)
-		require.NotZero(t, fromEntry.ID)
-		require.NotZero(t, fromEntry.CreatedAt)
-
-		_, err =store.GetEntry(context.Background(), fromEntry.ID)
-		require.NoError(t, err)
-
-		toEntry := result.ToEntry
-		require.NotEmpty(t, toEntry)
-		require.Equal(t, account2.ID, toEntry.AccountID)
-		require.Equal(t, amount, toEntry.Amount)
-		require.NotZero(t, toEntry.ID)
-		require.NotZero(t, toEntry.CreatedAt)
-
-		_, err =store.GetEntry(context.Background(), toEntry.ID)
-		require.NoError(t, err)
 	}
-}
+
+	// check the final updated balances
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account1.Balance, updatedAccount2.Balance)
+} 
